2408ba9217ee3d014938c630cf4fd686ecb17f2f
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-08-06 15:03:05 -0400

    Commit-all

---
+++ src/components/StudentData.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import './Styles.css'
+ 
+function TableData () {
+    const [data, getData] = useState([])
+    const URL = 'https://dummy.ngbots.com/student/by-state/Delaware';
+ 
+    useEffect(() => {
+        fetchData()
+    }, [])
+ 
+ 
+    const fetchData = () => {
+        fetch(URL)
+            .then((res) =>
+                res.json())
+ 
+            .then((response) => {
+                console.log(response);
+                getData(response);
+            })
+ 
+    }
+ 
+    return (
+        <>
+            <h1>Displaying the details of students in the state of Delaware</h1>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>SSN</th>
+                    <th>University</th>
+                    <th>Address</th>
+                </tr>
+                {data.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.name}</td>
+                        <td>{item.ssn}</td>
+                        <td>{item.university}</td>
+                    </tr>
+                ))}
+            </tbody>
+ 
+        </>
+    );
+}
+ 
+export default TableData;
